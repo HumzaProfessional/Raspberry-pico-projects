f8d9209bb45a81e9cf0d95555db8093427dcd0e7
Humza Rana <142959387+HumzaProfessional@users.noreply.github.com>
2023-09-04 18:17:24 -0400

    Update led.py

--- led.py
+++ led.py
@@ -1,14 +1,14 @@
 from machine import Pin
 import time
 
-led = Pin(25, Pin.OUT)
+led = Pin(25, Pin.OUT) # 25 is the pin the led's negative terminal is connnected to. Pin.OUT is any ground pin.
 
-led.on()
+led.on() # Led turns on for the first time
 
-time.sleep(.25)
-led.off()
-time.sleep(.25)
-led.on()
+time.sleep(.25) # Led stay on for .25 seconds
+led.off() # Led turns off
+time.sleep(.25) # Led stays off for .25 seconds
+led.on() # Led turns on again
 time.sleep(.25)
 led.off()
 time.sleep( .25)
